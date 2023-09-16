2e98b4899181a23489c9f82b3bd20bd5673b73b7
Pratikshakulkar <136431083+Pratikshakulkar@users.noreply.github.com>
2023-09-16 14:37:02 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -69,4 +69,4 @@ if st.button('Show Recommendation'):
         st.image(recommended_movie_posters[4])
     with col7:
         st.text(recommended_movie_names[6])
-        st.image(recommended_movie_posters[6)
+        st.image(recommended_movie_posters[6])
